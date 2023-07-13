26401fea8de775c63872a82331b6ee24309b00ad
Jremiez <6411130027@mut.ac.th>
2023-07-13 15:05:52 +0700

    Lutfy

--- menu.php
+++ menu.php
@@ -1,7 +1,6 @@
-<?php echo "My name's Patiphat"; ?>
-<?php echo "My surname's Jansamrit"; ?>
+<?php echo "6411130048"; ?>
+<?php echo "Pawee muhammad; ?>
 <?php echo "Im live in BKK"; ?>
 <?php echo "Mahanakorn"; ?>
 <?php echo "Nongjok"; ?>
-<?php echo "Im single"; ?>
-<?php echo "I want to be Hokage"; ?>
+
